f9d13e8a56426607aed445b323322f7b7b18718f
Andris Vītols <andrim.vitoam@gmail.com>
2025-03-25 21:15:54 +0200

    updating config

--- train.py
+++ train.py
@@ -5,12 +5,12 @@ from trainer import Trainer
 device = 'cuda:0'
 
 base_model = HookedTransformer.from_pretrained(
-    "gemma-2-2b", 
+    "Qwen/Qwen2.5-0.5B", 
     device=device, 
 )
 
 chat_model = HookedTransformer.from_pretrained(
-    "gemma-2-2b-it", 
+    "Qwen/Qwen2.5-0.5B-Instruct", 
     device=device, 
 )
 
@@ -24,23 +24,23 @@ default_cfg = {
     "buffer_mult": 128,
     "lr": 5e-5,
     "num_tokens": 400_000_000,
-    "l1_coeff": 2,
-    "beta1": 0.9,
-    "beta2": 0.999,
+    "l1_coeff": 2, # maybe use 4? Seems a little low.
+    "beta1": 0.9, # Default
+    "beta2": 0.999, # defualt
     "d_in": base_model.cfg.d_model,
     "dict_size": 2**14,
-    "seq_len": 1024,
+    "seq_len": 512, # using 512, maybe use 1024 in the future
     "enc_dtype": "fp32",
-    "model_name": "gemma-2-2b",
+    "model_name": "Qwen/Qwen2.5-0.5B",
     "site": "resid_pre",
     "device": "cuda:0",
-    "model_batch_size": 4,
+    "model_batch_size": 4, 
     "log_every": 100,
     "save_every": 30000,
     "dec_init_norm": 0.08,
-    "hook_point": "blocks.14.hook_resid_pre",
-    "wandb_project": "YOUR_WANDB_PROJECT",
-    "wandb_entity": "YOUR_WANDB_ENTITY",
+    "hook_point": "blocks.14.hook_resid_pre", # figure out which hookpoint to add crosscoder
+    "wandb_project": "Qwen-crosscoders",
+    "wandb_entity": "Qwen2.5-0.5B-Qwen2.5-0.5B-it-crosscoder",
 }
 cfg = arg_parse_update_cfg(default_cfg)
 
